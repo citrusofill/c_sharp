d90b496758b08848518ca307fbf466ffe4de67a9
SKuzovkov <citrusofill@gmail.com>
2022-12-21 09:56:04 +0300

    ДЗ к уроку 1

---
+++ lesson1/program2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+Console.WriteLine("Введите первое число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int d = Convert.ToInt32(Console.ReadLine());
+
+
+if (c>d)
+Console.WriteLine("число: "+ c + " больше чем число: "+ d);
+else if (c<d)
+Console.WriteLine("число: "+ d + " больше чем число: "+ c);
+else
+Console.WriteLine("число: "+ c + " равно числу: "+ d);
---
+++ lesson1/program4/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b & a>c)
+Console.WriteLine("Наибольшее число: " + a);
+
+if (b>a & b>c)
+Console.WriteLine("Наибольшее число: " + b);
+
+if (c>a & c>b)
+Console.WriteLine("Наибольшее число: " + c);
