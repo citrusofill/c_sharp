102174298b83b0ba1264f82f816d2dfcfc6aae96
SKuzovkov <citrusofill@gmail.com>
2023-01-25 10:47:29 +0300

    ДЗ к уроку 6

---
+++ lesson6/homework/41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int [] array = Console.ReadLine().Split(" ").Select(x=> int.Parse(x)).ToArray();
+int numbersGreaterZero = 0;
+foreach (int i in array)
+{
+    if (i > 0) numbersGreaterZero ++;
+}
+
+Console.WriteLine (numbersGreaterZero);
---
+++ lesson6/homework/43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2
+// задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Line (int [] coefficient, string name)
+{
+Console.WriteLine ($"Введите коэффициенты  (b k) для линии: {name}");
+coefficient = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.Clear();
+}
+
+
+int [] line1 = new int [2];
+int [] line2 = new int [2];
+
+Line (line1, "Прямая 1");
+Line (line2, "Прямая 2");
+
+Console.WriteLine($"[{string.Join(", ", line1)}]");
+Console.WriteLine($"[{string.Join(", ", line2)}]");
+
+//double crossX = (line1[0]-line2[0])/(line2[1]-line1[1]);
+//double crossY = crossX*line1[1]+line1[0];
+
+//Console.WriteLine ($"Координаты точки пересечения: Х= {crossX}; Y= {crossY}");
