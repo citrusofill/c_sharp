11154edf6862814ff7531bfc81946a610b81d11d
SKuzovkov <citrusofill@gmail.com>
2023-01-28 07:15:35 +0300

    Внес исправления в задачу 43

--- lesson6/homework/43/Program.cs
+++ lesson6/homework/43/Program.cs
@@ -4,16 +4,18 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-void Line (int [] coefficient, string name)
+void Line (double [] coefficient, string name)
 {
 Console.WriteLine ($"Введите коэффициенты  (b k) для линии: {name}");
-coefficient = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[] temp = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+for (int i = 0;i <= 1; i++)
+coefficient[i] = temp[i];
 Console.Clear();
 }
 
 
-int [] line1 = new int [2];
-int [] line2 = new int [2];
+double [] line1 = new double [2];
+double [] line2 = new double [2];
 
 Line (line1, "Прямая 1");
 Line (line2, "Прямая 2");
@@ -21,7 +23,7 @@ Line (line2, "Прямая 2");
 Console.WriteLine($"[{string.Join(", ", line1)}]");
 Console.WriteLine($"[{string.Join(", ", line2)}]");
 
-//double crossX = (line1[0]-line2[0])/(line2[1]-line1[1]);
-//double crossY = crossX*line1[1]+line1[0];
+double crossX = (line1[0]-line2[0])/(line2[1]-line1[1]);
+double crossY = crossX*line1[1]+line1[0];
 
-//Console.WriteLine ($"Координаты точки пересечения: Х= {crossX}; Y= {crossY}");
+Console.WriteLine ($"Координаты точки пересечения: Х= {crossX}; Y= {crossY}");
