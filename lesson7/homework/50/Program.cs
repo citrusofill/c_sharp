a97356e3bbd09cf1f66b088bf1c64c49fc470de8
SKuzovkov <citrusofill@gmail.com>
2023-02-01 00:38:44 +0300

    ДЗ к уроку 7

---
+++ lesson7/homework/50/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает 
+// позиции элемента в двумерном массиве, и возвращает значение 
+// этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void InputMatrix (int [,] matrix)
+{
+    for (int i = 0; i<matrix.GetLength(0);i++)
+    {
+        for (int j = 0; j<matrix.GetLength(1);j++)
+        {
+            matrix[i,j] = new Random().Next (-10,11); //-10, +10
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine ();
+    }
+}
+
+void FindNumber (int [,] matrix, int[]position)
+{
+if ((position[0]<= matrix.GetLength(0)) && (position[1]<= matrix.GetLength(1)))
+{
+    Console.WriteLine ($"На позиции {position[0]}, {position[1]}: {matrix [position[0],position[1]]}");
+    return;
+}
+Console.WriteLine("Такой позиции в матрице нет.");
+}
+
+
+
+Console.WriteLine ("Введите размер матрицы m x n:");
+int [] size = Console.ReadLine().Split (" ").Select (x=>int.Parse(x)).ToArray();
+int [,] matrix = new int [size[0],size[1]];
+Console.Clear();
+InputMatrix(matrix);
+
+Console.WriteLine ("Введите позицию элемента в матрице m,n:");
+int [] position = Console.ReadLine().Split (" ").Select (x=>int.Parse(x)).ToArray();
+FindNumber(matrix,position);
---
+++ lesson7/homework/52/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void InputMatrix (int [,] matrix)
+{
+    for (int i = 0; i<matrix.GetLength(0);i++)
+    {
+        for (int j = 0; j<matrix.GetLength(1);j++)
+        {
+            matrix[i,j] = new Random().Next (-10,11); //-10, +10
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine ();
+    }
+}
+
+void FindAverageColumn (int [,] matrix)
+{
+       for (int j = 0; j<matrix.GetLength(1);j++)
+    {
+    double summ = 0;
+    double averageColumn = 0;
+        for (int i = 0; i<matrix.GetLength(0);i++)
+        {
+            summ = summ + matrix[i,j];
+        }
+    averageColumn = summ/matrix.GetLength(0);
+    Console.WriteLine ($"Среднее арфиметическое столбца: {j} = {averageColumn}");
+    }
+}
+
+
+
+Console.WriteLine ("Введите размер матрицы m x n:");
+int [] size = Console.ReadLine().Split (" ").Select (x=>int.Parse(x)).ToArray();
+int [,] matrix = new int [size[0],size[1]];
+Console.Clear();
+InputMatrix(matrix);
+FindAverageColumn(matrix);
