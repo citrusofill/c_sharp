09f95423710e1760c8976f06d2e4789efc920603
citrusofill <citrusofill@gmail.com>
2023-02-18 21:20:38 +0300

    add homeworkfor lesson9

---
+++ lesson9/66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Summ (int M, int N)
+{
+int summa = 0;
+if (M<=N) summa = M + Summ (M+1, N);
+return (summa);
+}
+
+Console.WriteLine ("Введите значения M и N:");
+int [] size = Console.ReadLine().Split (" ").Select (x=>int.Parse(x)).ToArray();
+int summa = Summ (size[0], size[1]);
+Console.WriteLine ($"Cумма числел между M={size[0]} и N={size[1]} : {Summ (size[0], size[1])}");
+
+
+
---
+++ lesson9/homework/68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int Akker (int n, int m)
+{
+while (n>0)
+{
+    if (m==0) m=1;
+    else m = Akker(n,m-1);
+    n--;
+}
+return m+1;
+}
+
+Console.WriteLine ("Введите значения M и N:");
+int [] size = Console.ReadLine().Split (" ").Select (x=>int.Parse(x)).ToArray();
+Console.WriteLine ($"Функция Аккермана при M={size[0]} и N={size[1]} : {Akker (size[0], size[1])}");
